338273fcd2a3c424058055fe5bbf3b51d26d4938
WONG <lhwongae@lbe014.ust.hk>
2015-11-20 13:57:18 +0800

    Initial Commit

---
+++ CodeBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeBook
+//
+//  Created by WONG, Lai Hong on 20/11/15.
+//  Copyright (c) 2015 FYP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
